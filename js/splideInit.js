0d675898915a6f31fc186f99bcfc584dc11407df
arsenidze <melihovarsenij@gmail.com>
2023-10-23 22:39:49 +0300

    update fonts; update structure of nashi-teplytsi

---
+++ js/splideInit.js
@@ -0,0 +1,7 @@
+// https://splidejs.com/v3/guides/getting-started/
+document.addEventListener('DOMContentLoaded', function () {
+  const splide = new Splide('.splide', {
+    type: 'loop'
+  });
+  splide.mount();
+})
